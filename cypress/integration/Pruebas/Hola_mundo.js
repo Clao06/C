850aa56182f88626e00eac9e9c842e88d7e74bc9
Clao06 <claudia.diaz.mt@correo.usa.edu.co>
2021-10-15 13:49:50 -0500

    first commit

---
+++ cypress/integration/Pruebas/Hola_mundo.js
@@ -0,0 +1,12 @@
+describe("Prueba automatizada 1", () =>{
+ 
+    //Open Cypress|Set ".only"    
+   // it("Mi primer test -> Hola mundo", () => {
+      //  cy.log("Hola mundo")
+
+    it("Test Validar el título", () => {
+        cy.visit("https://www.activa-it.net/Login.aspx")    
+    })
+   
+
+})
